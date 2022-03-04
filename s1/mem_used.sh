90e89d430e95cf267693ee96bb8f1fa1cf0dd992
Your Name <you@example.com>
2022-03-04 08:36:50 +0000

    s1

--- s1/mem_used.sh
+++ s1/mem_used.sh
@@ -3,4 +3,4 @@ mem_used=`free -m | grep ^Mem | awk '{print $3}'`
 mem_total=`free -m | grep ^Mem | awk '{print $2}'`
 mem_percent=$((mem_used*100/mem_total))
 
-echo "mem used: $mem_percent%"
+echo "mem used: ${mem_percent}%"
